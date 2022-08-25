92e62a1dc080e4927b4784dacf576a260f0733db
Hector Haro H <hector8.haro@gmail.com>
2022-08-24 22:47:35 -0500

    clases2

--- 07-clases.js
+++ 07-clases.js
@@ -13,3 +13,36 @@ class User {
 // console.log(hhh.greeting());
 const horus = new User();
 console.log(horus.greeting2());
+
+// constructor
+
+class User {
+  // Constructor
+  constructor() {
+    console.log("Nuevo Usuario");
+  }
+  greeting() {
+    return "Hello Kunfu Panda";
+  }
+}
+
+const newhhh = new User();
+
+// this
+
+class User {
+  constructor(name) {
+    this.name = name;
+  }
+  // metodos
+  speak() {
+    return "Hello Perro";
+  }
+  greeting() {
+    return `${this.speak()} ${this.name}`;
+  }
+}
+
+
+const hector = new User('Hector');
+console.log(hector.greeting());
