1c5187b6b5c18d4a605fe505462820775ec1a5cd
Hector Haro H <hector8.haro@gmail.com>
2022-09-01 17:00:19 -0500

    es11

---
+++ src/es11/00-optional-chaining.js
@@ -0,0 +1,13 @@
+// optional chaining
+const users = {
+  hhh: {
+    country: "MX",
+    age: 32,
+  },
+  gndx: {
+    country: "CO",
+  },
+};
+
+console.log(users.hhh.country);
+console.log(users.bebeloper.country)
